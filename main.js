a39fbe55d7294854e43a2948bbcaf5f772313fc1
AlicePisciuneri <alice.corleto@libero.it>
2025-07-20 16:34:20 +0200

    pari e dispari

--- main.js
+++ main.js
@@ -1,48 +1,31 @@
-//Esercizio parola Palidroma
-//Chiedere all’utente di inserire una parola
-//Creare una funzione per capire se la parola inserita è palindroma
-
-
-// 1. Chiedo la parola all’utente
-const parolaUtente = prompt("Inserisci una parola:");
-
-// 2. Funzione  che controlla se è palindroma
-function èPalindroma(parola) {
-  // Creo una variabile dove metto la parola al contrario
-  let parolaInvertita = "";
-
-  // Ciclo per costruire la parola al contrario
-  for (let i = parola.length - 1; i >= 0; i--) {
-    parolaInvertita += parola[i];
-  }
-
-  // Confronto parola originale e parola invertita
-  if (parola === parolaInvertita) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// 3. Funzione  per stampare il risultato
-function stampaRisultato(esito) {
-  if (esito) {
-    console.log("La parola è palindroma!");
-  } else {
-    console.log(" La parola NON è palindroma.");
-  }
-}
-
-// 4. Uso le due funzioni
-const risultato = èPalindroma(parolaUtente); // true o false
-stampaRisultato(risultato); // stampa il messaggio
-
-
-
-
 
 //Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+const userChoice = prompt("Scegli: pari o dispari");
+const numeroUtente = parseInt(prompt("Scegli un numero da 1 a 5"));
+
+
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+function numeroRandom() {
+  return Math.floor(Math.random() * 5) + 1;
+}
+const numeroComputer = numeroRandom();
 //Sommiamo i due numeri
+const somma = numeroUtente + numeroComputer;
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+function pariDispari(numero) {
+  if (numero % 2 === 0) {
+    return "pari";
+  } else {
+    return "dispari";
+  }
+}
 //Dichiariamo chi ha vinto.
+if (sceltaUtente === risultato) {
+  console.log("Hai vinto!");
+} else {
+  console.log("Hai perso!");
+}
+
+
+
+
